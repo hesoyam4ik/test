9b5eaf7f0adee7629636fed5dff02965e884a1ef
hesoyam4ik <96014223+hesoyam4ik@users.noreply.github.com>
2021-12-12 17:39:02 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -50,9 +50,9 @@ jQuery(document).ready(function() {
  		new TradingView.widget(
 		 	{
 		  		"autosize": true,
-			  	"symbol": "NASDAQ:GAZP",
+			  	"symbol": "NASDAQ:GZPFY",
 			  	"interval": "D",
-			  	"timezone": "Etc/UTC",
+			  	// "timezone": "Etc/UTC",
 			  	"theme": "Dark",
 			  	"style": "1",
 			  	"locale": "en",
